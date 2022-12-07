daded3d3864f01638d5fc108b5ff480d0f0ac69a
Ümit Kaya <33umitkaya@gmail.com>
2022-12-07 12:03:23 +0300

    getElementsByName() Özelliğinin kullanım örneği

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const btnDomModule = document.getElementById('btnDomModule');
 btnDomModule.addEventListener('click', () => {
     domModule.get_SimpleFunction();
 });
+const btnDomModule_sec = document.getElementById('btnDomModule_sec');
+btnDomModule_sec.addEventListener('click', () => {
+    domModule.get_hepsinisec_SimpleFunction();
+});
+const btnDomModule_temizle = document.getElementById('btnDomModule_temizle');
+btnDomModule_temizle.addEventListener('click', () => {
+    domModule.get_hepsinitemizle_SimpleFunction();
+});
--- modules/dom-Module.js
+++ modules/dom-Module.js
@@ -3,6 +3,22 @@
 function get_SimpleFunction() {
     document.getElementById("demo").innerHTML = "ÜMİT KAYA";
 };
+//! getElementsByName(): Kendisine parametre olarak verilen değer doğrultusunda belge içerisinde belirtilen Name özelliğine sahip tüm elemanların referansını elde etmek için kullanılır. Eleman referansları birden fazla ise her bir eleman döngüyle elde edilebilir.
+function get_hepsinisec_SimpleFunction() {
+    var values = document.getElementsByName("ilgialanlari");
+    for (var i = 0; i < values.length; i++) {
+        values[i].checked = true;
+    }
+};
+function get_hepsinitemizle_SimpleFunction() {
+    var values = document.getElementsByName("ilgialanlari");
+    for (var i = 0; i < values.length; i++) {
+        values[i].checked = false;
+    }
+};
+
 export {
     get_SimpleFunction,
+    get_hepsinisec_SimpleFunction,
+    get_hepsinitemizle_SimpleFunction
 };
