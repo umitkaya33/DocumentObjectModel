9854f690b0e1f3e04c06a5ea10e56abfec4810ec
Ümit Kaya <33umitkaya@gmail.com>
2022-12-10 22:19:44 +0300

    Dom Attributes Module eklendi. app.js 'e entegre edildi..

--- app.js
+++ app.js
@@ -1,167 +1,167 @@
-import * as  domModule from "./modules/dom-Document-Module.js";
+import * as  domDocumentModule from "./modules/dom-Document-Module.js";
+import * as domAttributesModule from "./modules/dom-Attributes-Module.js"
 
 const btnDomModule = document.getElementById('btnDomModule');
 btnDomModule.addEventListener('click', () => {
-    domModule.get_SimpleFunction();
+    domDocumentModule.get_SimpleFunction();
 });
-
 const btnDomModule_sec = document.getElementById('btnDomModule_sec');
 btnDomModule_sec.addEventListener('click', () => {
-    domModule.get_hepsinisec_SimpleFunction();
+    domDocumentModule.get_hepsinisec_SimpleFunction();
 });
 const btnDomModule_temizle = document.getElementById('btnDomModule_temizle');
 btnDomModule_temizle.addEventListener('click', () => {
-    domModule.get_hepsinitemizle_SimpleFunction();
+    domDocumentModule.get_hepsinitemizle_SimpleFunction();
 });
 const btnDomModule_degistir = document.getElementById('btnDomModule_degistir');
 btnDomModule_degistir.addEventListener('click', () => {
-    domModule.get_degistir_SimpleFunction();
+    domDocumentModule.get_degistir_SimpleFunction();
 });
 const btnDomModule_arkaplan = document.getElementById('btnDomModule_arkaplan');
 btnDomModule_arkaplan.addEventListener('click', () => {
-    domModule.get_arkaplan_SimpleFunction();
+    domDocumentModule.get_arkaplan_SimpleFunction();
 });
 const btnRedBG = document.getElementById('btnRedBG');
 btnRedBG.addEventListener('click', () => {
-    domModule.SetColor('Red');
+    domDocumentModule.SetColor('Red');
 });
 const btnBlackBG = document.getElementById('btnBlackBG');
 btnBlackBG.addEventListener('click', () => {
-    domModule.SetColor('Black');
+    domDocumentModule.SetColor('Black');
 });
 const btnBlueBG = document.getElementById('btnBlueBG');
 btnBlueBG.addEventListener('click', () => {
-    domModule.SetColor('Blue');
+    domDocumentModule.SetColor('Blue');
 });
 const btnGreenBG = document.getElementById('btnGreenBG');
 btnGreenBG.addEventListener('click', () => {
-    domModule.SetColor('Green');
+    domDocumentModule.SetColor('Green');
 });
 const btnWhiteBG = document.getElementById('btnWhiteBG');
 btnWhiteBG.addEventListener('click', () => {
-    domModule.SetColor('White');
+    domDocumentModule.SetColor('White');
 });
 const imagesSmall = document.getElementById('images_Small');
 imagesSmall.addEventListener('click', () => {
-    domModule.imagesFunction();
+    domDocumentModule.imagesFunction();
 });
 const linksAdd = document.getElementById('btnLinksAdd');
 linksAdd.addEventListener('click', () => {
-    domModule.linksAdd();
+    domDocumentModule.linksAdd();
 });
 const example_Doctype = document.getElementById('bDoctype');
 example_Doctype.addEventListener('click', () => {
-    domModule.doctypeFunctionSimple();
+    domDocumentModule.doctypeFunctionSimple();
 });
 const example_DocumentElement = document.getElementById('bDocumentElement');
 example_DocumentElement.addEventListener('click', () => {
-    domModule.documentElementFunctionSimple();
+    domDocumentModule.documentElementFunctionSimple();
 });
 const example_CharacterSet = document.getElementById('bCharacterSet');
 example_CharacterSet.addEventListener('click', () => {
-    domModule.characterSetFunctionSimple();
+    domDocumentModule.characterSetFunctionSimple();
 });
 const example_InputEncoding = document.getElementById('bInputEncoding');
 example_InputEncoding.addEventListener('click', () => {
-    domModule.inputEncodingFunctionSimple();
+    domDocumentModule.inputEncodingFunctionSimple();
 });
 const example_Title = document.getElementById('bTitle');
 example_Title.addEventListener('click', () => {
-    domModule.titleFunctionSimple();
+    domDocumentModule.titleFunctionSimple();
 });
 const example_CompatMode = document.getElementById('bCompatMode');
 example_CompatMode.addEventListener('click', () => {
-    domModule.compatModeFunctionSimple();
+    domDocumentModule.compatModeFunctionSimple();
 });
 const example_DefaultView = document.getElementById('bDefaultView');
 example_DefaultView.addEventListener('click', () => {
-    domModule.defaultViewFunctionSimple();
+    domDocumentModule.defaultViewFunctionSimple();
 });
 const example_DocumentURI = document.getElementById('bDocumentURI');
 example_DocumentURI.addEventListener('click', () => {
-    domModule.documentURIFunctionSimple();
+    domDocumentModule.documentURIFunctionSimple();
 });
 const example_BaseURI = document.getElementById('bBaseURI');
 example_BaseURI.addEventListener('click', () => {
-    domModule.baseURIFunctionSimple();
+    domDocumentModule.baseURIFunctionSimple();
 });
 const example_URL = document.getElementById('bURL');
 example_URL.addEventListener('click', () => {
-    domModule.URLFunctionSimple();
+    domDocumentModule.URLFunctionSimple();
 });
 const example_Domain = document.getElementById('bDomain');
 example_Domain.addEventListener('click', () => {
-    domModule.domainFunctionSimple();
+    domDocumentModule.domainFunctionSimple();
 });
 const example_LastModified = document.getElementById('bLastModified');
 example_LastModified.addEventListener('click', () => {
-    domModule.lastModifiedFunctionSimple();
+    domDocumentModule.lastModifiedFunctionSimple();
 });
 const example_ReadyState = document.getElementById('bReadyState');
 example_ReadyState.addEventListener('click', () => {
-    domModule.readyStateFunctionSimple();
+    domDocumentModule.readyStateFunctionSimple();
 });
 const example_DesignMode = document.getElementById('bDesignMode');
 example_DesignMode.addEventListener('click', () => {
-    domModule.designModeFunctionSimple();
+    domDocumentModule.designModeFunctionSimple();
 });
 const example_HasFocus = document.getElementById('bHasFocus');
 example_HasFocus.addEventListener('click', () => {
-    domModule.hasFocusFunctionSimple();
+    domDocumentModule.hasFocusFunctionSimple();
 });
 const example_Plugin = document.getElementById('bPlugin');
 example_Plugin.addEventListener('click', () => {
-    domModule.pluginFunctionSimple();
+    domDocumentModule.pluginFunctionSimple();
 });
 const example_Anchors = document.getElementById('bAnchors');
 example_Anchors.addEventListener('click', () => {
-    domModule.anchorsFunctionSimple();
+    domDocumentModule.anchorsFunctionSimple();
 });
 const example_Links = document.getElementById('bLinks');
 example_Links.addEventListener('click', () => {
-    domModule.linksFunctionSimple();
+    domDocumentModule.linksFunctionSimple();
 });
 const example_Scripts = document.getElementById('bScripts');
 example_Scripts.addEventListener('click', () => {
-    domModule.scriptsFunctionSimple();
+    domDocumentModule.scriptsFunctionSimple();
 });
 const example_Forms = document.getElementById('bForms');
 example_Forms.addEventListener('click', () => {
-    domModule.formsFunctionSimple();
+    domDocumentModule.formsFunctionSimple();
 });
 const example_Location = document.getElementById('btnLocation');
 example_Location.addEventListener('click', () => {
-    domModule.locationFunctionSimple();
+    domDocumentModule.locationFunctionSimple();
 });
 const example_createElement = document.getElementById('bCreateElement');
 example_createElement.addEventListener('click', () => {
-    domModule.createElementFunctionSimple();
+    domDocumentModule.createElementFunctionSimple();
 });
 const example_querySelector = document.getElementById('bQuerySelector');
 example_querySelector.addEventListener('click', () => {
-    domModule.querySelectorFunctionSimple();
+    domDocumentModule.querySelectorFunctionSimple();
 });
 const example_querySelectorAll = document.getElementById('bQuerySelectorAll');
 example_querySelectorAll.addEventListener('click', () => {
-    domModule.querySelectorAllFunctionSimple();
+    domDocumentModule.querySelectorAllFunctionSimple();
 });
 const example_adoptNode = document.getElementById('bAdoptNode');
 example_adoptNode.addEventListener('click', () => {
-    domModule.adoptNodeFunctionSimple();
+    domDocumentModule.adoptNodeFunctionSimple();
 });
 const example_importNode = document.getElementById('bImportNode');
 example_importNode.addEventListener('click', () => {
-    domModule.importNodeFunctionSimple();
+    domDocumentModule.importNodeFunctionSimple();
 });
 const example_normalize = document.getElementById('bNormalize');
 example_normalize.addEventListener('click', () => {
-    domModule.normalizeFunctionSimple();
+    domDocumentModule.normalizeFunctionSimple();
 });
 const example_cookie = document.getElementById('bCookie');
 example_cookie.addEventListener('click', () => {
-    domModule.cookieFunctionSimple();
+    domDocumentModule.cookieFunctionSimple();
 });
-
-
-
-
+const testModule = document.getElementById('testUmit');
+testModule.addEventListener('click', () => {
+    domAttributesModule.testSimpleFunction();
+});
---
+++ modules/dom-Attributes-Module.js
@@ -0,0 +1,7 @@
+//! getElementById(): Kendisine parametre olarak verilen değer doğrultusunda belge içerisindeki belirtilen ID özelliğine sahip elemanın referansını elde etmek için kullanılır.
+function testSimpleFunction() {
+    alert("Ümit Kaya");
+};
+export {
+    testSimpleFunction,
+};
